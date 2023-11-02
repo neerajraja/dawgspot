eedbe5d8029e64acda274a71fbd0b09e11abeae2
nchristian93 <84033250+nchristian93@users.noreply.github.com>
2023-11-02 15:28:24 -0400

    Create Login component

---
+++ src/login/Login.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+function Login() {
+    return (
+        <div id="login-container" className="container">
+            <h1 id="login-header">Login</h1>
+            <form>
+                <input type="text" placeholder="Username..." className="input-box" />
+                <br />
+                <input type="text" placeholder="Password..." className="input-box" />
+                <div className="login-buttons">
+                    <button className="generic-button" id="signup-button">Sign Up</button>
+                    <button className="generic-button" id="login-button">Login</button>
+                </div>
+            </form>
+        </div>
+    );
+}
+
+export default Login;
