e1373c708b96fe03df6a64ec507410678aaa11a2
nchristian93 <84033250+nchristian93@users.noreply.github.com>
2023-11-16 15:13:37 -0500

    Finish navbar and create fake login variable in Main.jsx

--- src/homebar/topbar.jsx
+++ src/homebar/topbar.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './topbar.css';
 
-function TopBar() {
+function TopBar(props) {
   return (
     <div className="top-bar">
-      <div className="logo">Dawspot</div>
-      <div className="logout">Logout</div>
+      <div className="logo">Dawgspot</div>
+      <div className="custom-link">View All Games</div>
+      { props.isAdmin
+        ? <div className="custom-link" id="admin-view">View Admin Panel</div> 
+        : <></>
+      }
+      <div className="custom-link" id="login-logout">{props.isAdmin ? "Logout" : "Login"}</div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,18 @@
-
 import TopBar from './homebar/topbar';
 import logo from './logo.svg';
+import Login from "./login/Login";
 
+// Variable to control whether user is logged in as Administrator.
+// Sent as a prop to the TopBar component (for now).
+const isAdmin = true;
 
 function Main() {
 
 
     return (
         <div>
-            <TopBar/>
+            <TopBar isAdmin={isAdmin}/>
+            <Login/>
         </div>
     )
 }
