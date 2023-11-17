71a180817f60f9a0a37aedbddd783f8ed62be23d
nchristian93 <84033250+nchristian93@users.noreply.github.com>
2023-11-17 12:40:05 -0500

    Edit topbar text

--- src/homebar/topbar.jsx
+++ src/homebar/topbar.jsx
@@ -5,7 +5,7 @@ function TopBar(props) {
   return (
     <div className="top-bar">
       <div className="logo">Dawgspot</div>
-      <div className="custom-link">View All Games</div>
+      <div className="custom-link">Add New Game</div>
       { props.isAdmin
         ? <div className="custom-link" id="admin-view">View Admin Panel</div> 
         : <></> // Optionally render this element based on the value of props.isAdmin 
