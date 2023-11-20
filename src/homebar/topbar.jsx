93cf6984b0b407fe6ac0dc88ea89c6aab9a28812
nchristian93 <84033250+nchristian93@users.noreply.github.com>
2023-11-20 15:11:47 -0500

    Add Routing to topbar component

--- src/homebar/topbar.jsx
+++ src/homebar/topbar.jsx
@@ -1,16 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './topbar.css';
 
 function TopBar(props) {
   return (
     <div className="top-bar">
-      <div className="logo">Dawgspot</div>
-      <div className="custom-link">Add New Game</div>
+      <Link to="/" className="logo">Dawgspot</Link>
+      
       { props.isAdmin
-        ? <div className="custom-link" id="admin-view">View Admin Panel</div> 
+        ? <div className="custom-link" id="admin-view">Add New Game</div> 
         : <></> // Optionally render this element based on the value of props.isAdmin 
       }
-      <div className="custom-link" id="login-logout">{props.isAdmin ? "Logout" : "Login"}</div>
+      { props.isAdmin
+        ? <Link to='/' className="custom-link">Logout</Link>
+        : <>
+            <Link to='/signup' className="custom-link">Sign Up</Link>
+            <Link to='/login' className="custom-link">Login</Link>            
+          </>
+      }
     </div>
   );
 }
