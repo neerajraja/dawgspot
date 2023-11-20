ad0846612df57b7cc03b75589e18c2b771ff75b4
nchristian93 <84033250+nchristian93@users.noreply.github.com>
2023-11-20 14:38:31 -0500

    Set up Routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
 import TopBar from './homebar/topbar';
 import HomePage from './homepage/HomePage';
+import SignUp from './signup/SignUp';
+import Login from './login/Login';
 
 // Variable to control whether user is logged in (as Administrator).
 // Sent as a prop to any components that render differently based on admin status.
@@ -8,10 +12,19 @@ const isAdmin = true;
 
 function App() {
     return(
-        <div>
-            <TopBar isAdmin={isAdmin} />
-            <HomePage isAdmin={isAdmin} />
-        </div>
+        <Router>
+            <div>
+                <TopBar isAdmin={isAdmin} />
+                <Routes>
+                    <Route exact path='/' element={<HomePage isAdmin={isAdmin} />} />
+                    <Route path='/signup' element={<SignUp />} />
+                    <Route path='/login' element={<Login />} />
+                    { /* TODO: Add routes for AddItem component when it is completed. */ }
+
+                    <Route path='*' element={<p>404 Page not Found</p>} />
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
