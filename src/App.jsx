fb9f8f3fc33f943537ace8731356a2df14ae6c71
nchristian93 <84033250+nchristian93@users.noreply.github.com>
2023-11-20 14:27:55 -0500

    Merge main.jsx and App.jsx into one file

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,18 @@
-import './App.css';
-import Main from './main'
-import {createBrowserRouter, RouterProvider} from "react-router-dom";
+import React from 'react';
+import TopBar from './homebar/topbar';
+import HomePage from './homepage/HomePage';
 
-
-
-
+// Variable to control whether user is logged in (as Administrator).
+// Sent as a prop to any components that render differently based on admin status.
+const isAdmin = true;
 
 function App() {
-  const router = createBrowserRouter([{
-    path: "/",
-    element: <Main/>,
-
-  }
-
-  ])
-  return (
-    <RouterProvider router={router}/>
-  );
+    return(
+        <div>
+            <TopBar isAdmin={isAdmin} />
+            <HomePage isAdmin={isAdmin} />
+        </div>
+    );
 }
 
 export default App;
