8607607874b3267854302f10370927cc5160048c
nchristian93 <84033250+nchristian93@users.noreply.github.com>
2023-11-20 22:50:23 -0500

    Update AddItem to include new fields

--- src/additem/AddItem.jsx
+++ src/additem/AddItem.jsx
@@ -4,14 +4,16 @@ import './AddItem.css';
 function AddItem() {
     const [homeTeam, setHomeTeam] = useState('');
     const [awayTeam, setAwayTeam] = useState('');
-    const [homeScore, setHomeScore] = useState(0);
-    const [awayScore, setAwayScore] = useState(0);
+    const [homeRank, setHomeRank] = useState('');
+    const [awayRank, setAwayRank] = useState('');
+    const [homeOdds, setHomeOdds] = useState('');
     const [gameImage, setGameImage] = useState('');
 
     const homeTeamChangeHandler = event => setHomeTeam(event.target.value);
     const awayTeamChangeHandler = event => setAwayTeam(event.target.value);
-    const homeScoreChangeHandler = event => setHomeScore(event.target.value);
-    const awayScoreChangeHandler = event => setAwayScore(event.target.value);
+    const homeRankChangeHandler = event => setHomeRank(event.target.value);
+    const awayRankChangeHandler = event => setAwayRank(event.target.value);
+    const homeOddsChangeHandler = event => setHomeOdds(event.target.value);
     const gameImageChangeHandler = event => setGameImage(event.target.value);
 
     function submitHandler(event) {
@@ -21,8 +23,9 @@ function AddItem() {
             id: Math.random().toString(),
             homeTeam: homeTeam,
             awayTeam: awayTeam,
-            homeScore: homeScore,
-            awayScore: awayScore,
+            homeRank: homeRank,
+            awayRank: awayRank,
+            homeOdds: homeOdds,
             gameImage: gameImage
         };
 
@@ -30,8 +33,9 @@ function AddItem() {
 
         setHomeTeam('');
         setAwayTeam('');
-        setHomeScore(0);
-        setAwayScore(0);
+        setHomeRank('');
+        setAwayRank('');
+        setHomeOdds('');
         setGameImage('');
     }
 
@@ -41,8 +45,9 @@ function AddItem() {
             <form className="additem-form" onSubmit={submitHandler}>
                 <input type="text" id="additem-hometeam" placeholder="Home Team name.." value={homeTeam} onChange={homeTeamChangeHandler}/>
                 <input type="text" id="additem-awayteam" placeholder="Away Team name.." value={awayTeam} onChange={awayTeamChangeHandler}/>
-                <input type="number" id="additem-homescore" placeholder="Home Team score.." value={homeScore} onChange={homeScoreChangeHandler}/>
-                <input type="number" id="additem-awayscore" placeholder="Away Team score.." value={awayScore} onChange={awayScoreChangeHandler}/>
+                <input type="text" id="additem-homerank" placeholder="Home Team rank.." value={homeRank} onChange={homeRankChangeHandler}/>
+                <input type="text" id="additem-awayrank" placeholder="Away Team rank.." value={awayRank} onChange={awayRankChangeHandler}/>
+                <input type="text" id="additem-homeodds" placeholder="Home Team odds.." value={homeOdds} onChange={homeOddsChangeHandler}/>
                 <input type="text" id="additem-gameimage" placeholder="URL to game image.." value={gameImage} onChange={gameImageChangeHandler}/>
                 <button className="generic-button" type="submit" id="additem-button">Add Item</button>
             </form>
