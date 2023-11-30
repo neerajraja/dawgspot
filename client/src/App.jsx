864a87577d762e5686b53fc0d4b6db154ce5988a
nchristian93 <84033250+nchristian93@users.noreply.github.com>
2023-11-30 18:09:54 -0500

    Fix axios calls to backend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import axios from 'axios';
+import UserContext from './context/UserContext';
 
 import TopBar from './homebar/topbar';
 import HomePage from './homepage/HomePage';
@@ -13,7 +16,41 @@ function App() {
     const [isAdmin, setIsAdmin] = useState(false);
     const toggleIsAdmin = prevIsAdmin => setIsAdmin(!prevIsAdmin);
 
+    const [userData, setUserData] = useState({
+        token: undefined,
+        user: undefined
+    });
+
+    useEffect(() => {
+        const checkLoggedIn = async () => {
+            let token = localStorage.getItem('auth-token');
+            if (token === null) {
+                localStorage.setItem('auth-token', '');
+                token = '';
+            }
+
+            const tokenResponse = await axios.post(
+                'http://localhost:8089/api/users/tokenIsValid',
+                null,
+                { header: { 'x-auth-token': token } }
+            );
+
+            if (tokenResponse.data) {
+                const userRes = await axios.get('http://localhost:8089/api/users', {
+                    header: { 'x-auth-token': token },
+                });
+
+                setUserData({
+                    token,
+                    user: userRes.data,
+                });
+            }
+        };
+        checkLoggedIn();
+    }, []);
+
     return(
+        <UserContext.Provider value={{ userData, setUserData }}>
         <Router>
             <div>
                 <TopBar isAdmin={isAdmin} toggleIsAdmin={toggleIsAdmin}/>
@@ -27,6 +64,7 @@ function App() {
                 </Routes>
             </div>
         </Router>
+        </UserContext.Provider>
     );
 }
 
--- client/src/login/Login.jsx
+++ client/src/login/Login.jsx
@@ -1,21 +1,55 @@
-import React from "react";
-import { Link } from 'react-router-dom';
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from 'react-router-dom';
+
+import axios from 'axios';
+import UserContext from "../context/UserContext";
+
 import "./Login.css";
 
 function Login(props) {
 
-    function loginHandler(event) {
-        //event.preventDefault();
-        props.toggleIsAdmin();
-    }
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState();
+    const [loading, setLoading] = useState(false);
+    const navigate = useNavigate();
+    const {setUserData} = useContext(UserContext);
+
+    /**
+     * @function loginHandler - a function that handles login
+     * @param {Event} e - the event that triggered the function
+     * @returns {void} - returns nothing
+     * @description - logs in a user
+     * @version 0.0.1
+     * Last edited 11/30/2023 by Cannon M. Hutcheson & Nathan Christian
+    */
+    function loginHandler(e) {
+        e.preventDefault();
+        // props.toggleIsAdmin(); -- is this needed?
+        setLoading(true);
+        try {
+            const loginUser = { email, password };
+            const loginRes = axios.post("http://localhost:8082/api/users/login", loginUser);
+            setUserData({
+                token: loginRes.data.token,
+                user: loginRes.data.user
+            });
+            localStorage.setItem("auth-token", loginRes.data.token);
+            navigate("/");
+            // setLoading(false); -- this may need to be in the catch block
+        } catch(err) { 
+            setLoading(false);
+            err.response.data.msg && setError(err.response.data.msg);
+        } // try-catch
+    } // loginHandler
 
     return (
         <div id="login-container" className="container">
             <h1 id="login-header">Login</h1>
             <form onSubmit={loginHandler}>
-                <input id="login-username-box" type="text" placeholder="Username..." className="input-box" />
+                <input id="login-username-box" type="text" placeholder="Email..." className="input-box" required onChange={e => setEmail(e.target.value)}/>
                 <br />
-                <input id="login-password-box" type="password" placeholder="Password..." className="input-box" />
+                <input id="login-password-box" type="password" placeholder="Password..." className="input-box" required onChange={e => setPassword(e.target.value)}/>
                 <div className="login-buttons">
                     <Link to="/signup" className="generic-button" id="signup-button">Sign Up</Link>
                     <Link to="/" className="generic-button" id="login-button" onClick={loginHandler}>Login</Link>
